2356b9d92fe5a24986c8c710224b29107d0c7c0b
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-07-28 00:58:04 +0700

    1

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+
+export default function Main(){
+    return(
+        <main>
+            <h1 className = "heading-title">Fun facts about React</h1>
+            <ul className = "react-fact-list">
+                <li>Was released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src = '/react-icon-small.png' className = 'react-logo' width = "30px" height = "30px"/>
+            <h3 className = 'react-title'>ReactFacts</h3>
+            <h4 className = 'react-head'>React Course Project</h4>
+        </nav>
+    )
+}
